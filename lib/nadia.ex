7da4c68b266dfd728b9daf3bc8a78722f9d89ba6
John Mandalakas <jnmandal+if-you-know-me-use-my-real-email@cobarde.soy>
2018-02-09 17:32:58 -0600

    Add delete message impl

--- lib/nadia.ex
+++ lib/nadia.ex
@@ -560,6 +560,23 @@ defmodule Nadia do
   end
 
   @doc """
+  Use this method to delete a message, including service messages, with the following limitations:
+  * A message can only be deleted if it was sent less than 48 hours ago.
+  * Bots can delete outgoing messages in groups and supergroups.
+  * Bots granted can_post_messages permissions can delete outgoing messages in channels.
+  * If the bot is an administrator of a group, it can delete any message there.
+  * If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there.
+  Returns True on success.
+
+  Args:
+  * `chat_id` - Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+  * `message_id` - Identifier of the message to delete
+  """
+  def delete_message(chat_id, message_id) do
+    request("deleteMessage", [chat_id: chat_id, message_id: message_id])
+  end
+
+  @doc """
   Use this method to edit captions of messages sent by the bot or via
   the bot (for inline bots). On success, the edited Message is returned.
 
